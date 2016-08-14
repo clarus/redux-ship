09cf90cc3a673ba18409f094a82a4000ce029657
Guillaume Claret <dev@clarus.me>
2016-08-14 09:55:51 +0200

    Add constructors Wait, Call and All

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('hi');
+// @flow
+import * as Ship from './ship';
+
+function wait(ms: number): Promise<void> {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function waitWith<A>(ms: number, result: A): Promise<A> {
+  await wait(ms);
+  return result;
+}
+
+function* mainWait(): Ship.t<void> {
+  console.log('start wait');
+  const message = yield* Ship.wait2(waitWith, 3 * 1000, 'hi');
+  console.log(message);
+}
+
+function* mainCall(): Ship.t<void> {
+  console.log('start call');
+  yield* Ship.call(mainWait());
+}
+
+function* mainPar(): Ship.t<void> {
+  console.log('start par');
+  const messages = yield* Ship.all2(
+    Ship.wait2(waitWith, 3 * 1000, 'hi'),
+    Ship.wait0(waitWith(3 * 1000, 2))
+  );
+  console.log(messages);
+}
+
+async function main(): Promise<void> {
+  console.log('start');
+  const message = await waitWith(3 * 1000, 'hi');
+  console.log(message);
+  await Ship.run(mainWait());
+  await Ship.run(mainCall());
+  await Ship.run(mainPar());
+}
+
+main();
---
+++ src/ship.js
@@ -0,0 +1,86 @@
+// @flow
+
+export type Effect = {
+  type: 'Wait',
+  args: any[],
+  fn: (...args: any[]) => Promise<any>
+} | {
+  type: 'Call',
+  arg: Generator<Effect, any, any>
+} | {
+  type: 'All',
+  args: Generator<Effect, any, any>[]
+};
+
+export type t<A> = Generator<Effect, A, any>;
+
+export async function run<A>(ship: t<A>, answer?: any): Promise<A> {
+  const result = ship.next(answer);
+  if (result.done) {
+    return result.value;
+  }
+  switch (result.value.type) {
+    case 'Wait': {
+      const newAnswer = await result.value.fn(...result.value.args);
+      return run(ship, newAnswer);
+    }
+    case 'Call': {
+      const newAnswer = await run(result.value.arg);
+      return run(ship, newAnswer);
+    }
+    case 'All': {
+      const newAnswer = await Promise.all(result.value.args.map(run));
+      return run(ship, newAnswer);
+    }
+    default:
+      return result.value;
+  }
+}
+
+export function* waitn<A>(fn: (...args: any[]) => Promise<A>, args: any[]): t<A> {
+  const result = yield {
+    type: 'Wait',
+    args,
+    fn,
+  };
+  return (result: any);
+}
+
+export function* wait<A, B>(fn: (arg: A) => Promise<B>, arg: A): t<B> {
+  return yield* waitn(fn, [arg]);
+}
+
+export function* wait0<A>(arg: Promise<A>): t<A> {
+  return yield* waitn(() => arg, []);
+}
+
+export function* wait2<A1, A2, B>(
+  fn: (arg1: A1, arg2: A2) => Promise<B>,
+  arg1: A1, arg2: A2)
+  : t<B> {
+  return yield* waitn(fn, [arg1, arg2]);
+}
+
+export function* call<A>(arg: t<A>): t<A> {
+  const result = yield {
+    type: 'Call',
+    arg,
+  };
+  return (result: any);
+}
+
+export function* alln(args: t<any>[]): t<any[]> {
+  const result = yield {
+    type: 'All',
+    args,
+  };
+  return (result: any);
+}
+
+export function* all<A>(args: t<A>[]): t<A[]> {
+  return yield* alln(args);
+}
+
+export function* all2<A1, A2>(arg1: t<A1>, arg2: t<A2>): t<[A1, A2]> {
+  return yield* alln([arg1, arg2]);
+}
