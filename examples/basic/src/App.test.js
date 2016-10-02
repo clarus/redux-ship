c65c8630d943b375ecfb365f46de2a302c75efdc
Guillaume Claret <dev@clarus.me>
2016-10-02 15:06:44 +0200

    Fix the test

--- examples/basic/src/App.test.js
+++ examples/basic/src/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as Model from './model';
+
+function dispatch() {
+}
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App dispatch={dispatch} state={Model.initialState} />, div);
 });
