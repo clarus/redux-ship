0b5b7ca6d3bc9c166149362c0528e5fbfc0e3b58
Guillaume Claret <dev@clarus.me>
2016-10-02 13:50:09 +0200

    Add the beginning of a basic example

---
+++ examples/basic/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Redux Ship</h1>
+          <h2>Basic example</h2>
+        </div>
+        <p className="App-intro">
+          <button>
+            Get Luke description
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
