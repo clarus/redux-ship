ceff531bfdee8afc03df123badff928567f9521a
Guillaume Claret <dev@clarus.me>
2016-10-07 14:01:30 +0200

    Rename map* to lift*

--- src/index.js
+++ src/index.js
@@ -179,27 +179,27 @@ export function* getState<Effect, Action, State>(): t<Effect, Action, State, Sta
 }
 
 function* mapCommand<Effect1, Action1, State1, Effect2, Action2, State2>(
-  mapEffect: (effect1: Effect1) => t<Effect2, Action2, State2, any>,
-  mapAction: (action1: Action1) => Action2,
-  mapState: (state2: State2) => State1,
+  liftEffect: (effect1: Effect1) => t<Effect2, Action2, State2, any>,
+  liftAction: (action1: Action1) => Action2,
+  liftState: (state2: State2) => State1,
   command: Command<Effect1, Action1, State1>
 ): t<Effect2, Action2, State2, any> {
   switch (command.type) {
   case 'Effect':
-    return yield* mapEffect(command.effect);
+    return yield* liftEffect(command.effect);
   case 'Dispatch':
-    return yield* dispatch(mapAction(command.action));
+    return yield* dispatch(liftAction(command.action));
   case 'GetState':
-    return mapState(yield* getState());
+    return liftState(yield* getState());
   default:
     return command;
   }
 }
 
 function* mapWithAnswer<Effect1, Action1, State1, Effect2, Action2, State2, A>(
-  mapEffect: (effect1: Effect1) => t<Effect2, Action2, State2, any>,
-  mapAction: (action1: Action1) => Action2,
-  mapState: (state2: State2) => State1,
+  liftEffect: (effect1: Effect1) => t<Effect2, Action2, State2, any>,
+  liftAction: (action1: Action1) => Action2,
+  liftState: (state2: State2) => State1,
   ship: t<Effect1, Action1, State1, A>,
   answer?: any
 ): t<Effect2, Action2, State2, A> {
@@ -209,14 +209,14 @@ function* mapWithAnswer<Effect1, Action1, State1, Effect2, Action2, State2, A>(
   }
   switch (result.value.type) {
   case 'Command': {
-    const newAnswer = yield* mapCommand(mapEffect, mapAction, mapState, result.value.command);
-    return yield* mapWithAnswer(mapEffect, mapAction, mapState, ship, newAnswer);
+    const newAnswer = yield* mapCommand(liftEffect, liftAction, liftState, result.value.command);
+    return yield* mapWithAnswer(liftEffect, liftAction, liftState, ship, newAnswer);
   }
   case 'All': {
     const newAnswer = yield* allAny(...result.value.ships.map((currenyShip) =>
-      mapWithAnswer(mapEffect, mapAction, mapState, currenyShip)
+      mapWithAnswer(liftEffect, liftAction, liftState, currenyShip)
     ));
-    return yield* mapWithAnswer(mapEffect, mapAction, mapState, ship, newAnswer);
+    return yield* mapWithAnswer(liftEffect, liftAction, liftState, ship, newAnswer);
   }
   default:
     return result.value;
@@ -224,11 +224,11 @@ function* mapWithAnswer<Effect1, Action1, State1, Effect2, Action2, State2, A>(
 }
 
 export function map<Effect, Action1, State1, Action2, State2, A>(
-  mapAction: (action1: Action1) => Action2,
-  mapState: (state2: State2) => State1,
+  liftAction: (action1: Action1) => Action2,
+  liftState: (state2: State2) => State1,
   ship: t<Effect, Action1, State1, A>
 ): t<Effect, Action2, State2, A> {
-  return mapWithAnswer((effect) => call(effect), mapAction, mapState, ship);
+  return mapWithAnswer((effect) => call(effect), liftAction, liftState, ship);
 }
 
 export type SnapshotItem<Effect, Action, State> = {
