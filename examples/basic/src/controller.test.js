ef56da0b1db24f5cfe4aaaba7f223e6c9e8ef58f
Guillaume Claret <dev@clarus.me>
2016-10-08 14:36:56 +0200

    Add snapshot testing

--- examples/basic/src/controller.test.js
+++ examples/basic/src/controller.test.js
@@ -1,9 +1,11 @@
 // @flow
+import 'babel-polyfill';
+import * as Ship from 'redux-ship';
 import * as Controller from './controller';
 
-it('gets the full name', () => {
+it('gets the full name (by hand)', () => {
   const ship = Controller.control({
-    type: 'Load'
+    type: 'Load',
   });
   {
     const {value} = ship.next();
@@ -49,4 +51,12 @@ it('gets the full name', () => {
     const {done} = ship.next();
     expect(done).toBe(true);
   }
+});
+
+it('gets the full name (by snapshot)', () => {
+  const ship = Controller.control({
+    type: 'Load',
+  });
+  const snapshot = [{"type":"Dispatch","action":{"type":"LoadStart"}},{"type":"Effect","effect":{"type":"HttpRequest","url":"http://swapi.co/api/people/1/"},"result":"{\"name\":\"Luke Skywalker\",\"height\":\"172\",\"mass\":\"77\",\"hair_color\":\"blond\",\"skin_color\":\"fair\",\"eye_color\":\"blue\",\"birth_year\":\"19BBY\",\"gender\":\"male\",\"homeworld\":\"http://swapi.co/api/planets/1/\",\"films\":[\"http://swapi.co/api/films/6/\",\"http://swapi.co/api/films/3/\",\"http://swapi.co/api/films/2/\",\"http://swapi.co/api/films/1/\",\"http://swapi.co/api/films/7/\"],\"species\":[\"http://swapi.co/api/species/1/\"],\"vehicles\":[\"http://swapi.co/api/vehicles/14/\",\"http://swapi.co/api/vehicles/30/\"],\"starships\":[\"http://swapi.co/api/starships/12/\",\"http://swapi.co/api/starships/22/\"],\"created\":\"2014-12-09T13:50:51.644000Z\",\"edited\":\"2014-12-20T21:17:56.891000Z\",\"url\":\"http://swapi.co/api/people/1/\"}"},{"type":"Dispatch","action":{"type":"LoadSuccess","fullName":"Luke Skywalker"}}];
+  expect(Ship.simulate(ship, snapshot).snapshot).toEqual(snapshot);
 });
