0c7451afad60a6d69872db94155166937841df7a
Guillaume Claret <dev@clarus.me>
2016-10-20 13:38:08 +0200

    Remove outdated examples

--- examples/sharing/src/store.js
+++
@@ -1,9 +0,0 @@
-import {applyMiddleware, createStore} from 'redux';
-import createLogger from 'redux-logger';
-import * as Model from './model';
-
-const middlewares = [
-  createLogger(),
-];
-
-export default createStore(Model.reduce, Model.initialState, applyMiddleware(...middlewares));
