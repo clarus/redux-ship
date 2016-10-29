741a8c7e9b6991ce210753c801d82e9895dc29a6
Guillaume Claret <dev@clarus.me>
2016-10-29 17:42:54 +0200

    Completes the counter example

--- examples/counter/src/index.js
+++ examples/counter/src/index.js
@@ -1,9 +1,24 @@
+// @flow
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Ship from 'redux-ship';
+import {logControl} from 'redux-ship-logger';
 import App from './App';
 import './index.css';
+import * as Controller from './controller';
+import store from './store';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function dispatch(action: Controller.Action): void {
+  Ship.run(() => {}, store.dispatch, store.getState, logControl(Controller.control)(action));
+}
+
+function render(): void {
+  ReactDOM.render(
+    <App dispatch={dispatch} state={store.getState()} />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render);
+render();
