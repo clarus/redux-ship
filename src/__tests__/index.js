bfc9914115a0c1c7539d97628ae839adf5794a1b
Guillaume Claret <dev@clarus.me>
2017-01-08 15:33:00 +0100

    Add tests for the run function

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -30,13 +30,34 @@ type EyeCommit = {
   color: string,
 };
 
+export function eyeReduce(state: EyeState, commit: EyeCommit): EyeState {
+  switch (commit.type) {
+  case 'LoadStart':
+    return {
+      ...state,
+      isLoading: true,
+    };
+  case 'LoadSuccess':
+    return {
+      ...state,
+      color: commit.color,
+      isLoading: false,
+    };
+  default:
+    return state;
+  }
+}
+
 type EyeControl<A> = Ship.Ship<*, EyeCommit, EyeState, A>;
 
 function* eyeControl(): EyeControl<void> {
   const currentEyeColor = yield* Ship.getState(state => state.color);
   if (!currentEyeColor) {
     yield* Ship.commit({type: 'LoadStart'});
-    const r2d2 = yield* httpRequest('http://swapi.co/api/people/3/');
+    const [r2d2] = yield* Ship.all2(
+      httpRequest('http://swapi.co/api/people/3/'),
+      httpRequest('http://swapi.co/api/people/4/')
+    );
     const eyeColor = JSON.parse(r2d2).eye_color;
     yield* Ship.commit({type: 'LoadSuccess', color: eyeColor});
   }
@@ -67,11 +88,74 @@ function* control(): Control<void> {
 
 describe('map', () => {
   test('without eye', () => {
-    const snapshot = [{"type":"GetState"},{"type":"Commit","commit":{"type":"Eye","commit":{"type":"LoadStart"}}},{"type":"Effect","effect":{"type":"HttpRequest","url":"http://swapi.co/api/people/3/"},"result":"{\"name\":\"R2-D2\",\"eye_color\":\"red\"}"},{"type":"Commit","commit":{"type":"Eye","commit":{"type":"LoadSuccess","color":"red"}}}];
+    const effectSnapshot = (id, name, eyeColor) => ({
+      type: 'Effect',
+      effect: {
+        type: 'HttpRequest',
+        url: `http://swapi.co/api/people/${id}/`,
+      },
+      result: JSON.stringify({
+        eye_color: eyeColor,
+        name,
+      }),
+    });
+    const snapshot = [
+      {type: 'GetState'},
+      {type: 'Commit', commit: {type: 'Eye', commit: {type: 'LoadStart'}}},
+      {
+        type: 'All',
+        snapshots: [
+          [effectSnapshot('3', 'R2-D2', 'red')],
+          [effectSnapshot('4', 'Darth Vader', 'yellow')],
+        ],
+      },
+      {type: 'Commit', commit: {type: 'Eye', commit: {type: 'LoadSuccess', color: 'red'}}},
+    ];
     expect(Ship.simulate(control(), snapshot)).toEqual(snapshot);
   });
   test('with eye', () => {
-    const snapshot = [{"type":"GetState",state:"red"}];
+    const snapshot = [
+      {type: 'GetState', state: 'red'}
+    ];
     expect(Ship.simulate(control(), snapshot)).toEqual(snapshot);
+  });
+});
+
+describe('run', () => {
+  function runEffect(effect: Effect) {
+    switch (effect.type) {
+    case 'HttpRequest':
+      return JSON.stringify({
+        eye_color: 'red',
+        name: 'R2-D2',
+      });
+    default:
+      return;
+    }
+  }
+
+  function createStore(initialState: EyeState) {
+    let state = initialState;
+    return {
+      dispatch(commit) {
+        state = eyeReduce(state, commit);
+      },
+      getState() {
+        return state;
+      }
+    };
+  }
+
+  test('without eye', async () => {
+    const store = createStore(initialEyeState);
+    await Ship.run(runEffect, store, eyeControl());
+    expect(store.getState()).toMatchSnapshot();
+  });
+
+  test('twice', async () => {
+    const store = createStore(initialEyeState);
+    await Ship.run(runEffect, store, eyeControl());
+    await Ship.run(runEffect, store, eyeControl());
+    expect(store.getState()).toMatchSnapshot();
   });
 });
