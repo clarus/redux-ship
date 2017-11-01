ba56637bb48e57f5c1956456f875fc6dbeadb72a
Guillaume Claret <dev@clarus.me>
2017-11-01 22:59:26 +0100

    Add comments

--- examples/simple/src/effect.js
+++ examples/simple/src/effect.js
@@ -2,6 +2,8 @@
 import type {Ship} from 'redux-ship';
 import {call} from 'redux-ship';
 
+// The type of side-effects for the whole application. There should be just one file
+// of this kind for an application, with a list of effects kept as short as possible.
 export type Effect = {
   type: 'Delay',
   ms: number
@@ -10,6 +12,24 @@ export type Effect = {
   url: string,
 };
 
+// An effect constructor for the `delay`. The aim of an effect constructor is to
+// give the return type of an effect, in this case `void`.
+export function* delay<Commit, State>(ms: number): Ship<Effect, Commit, State, void> {
+  return yield* call({
+    type: 'Delay',
+    ms,
+  });
+}
+
+export function* httpRequest<Commit, State>(url: string): Ship<Effect, Commit, State, string> {
+  return yield* call({
+    type: 'HttpRequest',
+    url,
+  });
+}
+
+// The function to actually evaluate an effect. In a Redux Ship application, this should
+// be the only non-purely functional function!
 export async function run(effect: Effect): Promise<any> {
   switch (effect.type) {
   case 'Delay': {
@@ -23,18 +43,4 @@ export async function run(effect: Effect): Promise<any> {
   default:
     return;
   }
-}
-
-export function delay<Commit, State>(ms: number): Ship<Effect, Commit, State, void> {
-  return call({
-    type: 'Delay',
-    ms,
-  });
-}
-
-export function httpRequest<Commit, State>(url: string): Ship<Effect, Commit, State, string> {
-  return call({
-    type: 'HttpRequest',
-    url,
-  });
 }
--- examples/simple/src/model.js
+++ examples/simple/src/model.js
@@ -1,4 +1,10 @@
 // @flow
+// In this file we define the type of the state of our application and how it
+// can evolve. This is basically where we put all our Redux code. We prefer
+// to name the actions `commit`. Thus we differentiate the commits from the
+// actions dispatched by the React components, which are given to the Ship
+// middleware. Then the Ship middleware generates commits which are evaluated
+// by the reducer.
 
 export type State = {
   isLoading: bool,
